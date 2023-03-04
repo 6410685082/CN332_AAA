5b3be13ecde7cf8b31340ad08859b71836d00faf
Phattarakorn Kaewchukul <6310611030@cn103>
2023-03-05 00:27:11 +0700

    add admin.py

--- SmartTrafficMonitoring/user/admin.py
+++ SmartTrafficMonitoring/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserInfo, RoleID
 
-# Register your models here.
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'role_id_id', 'phone_number')
+
+class RoleIDAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(RoleID, RoleIDAdmin)
+admin.site.register(UserInfo, UserInfoAdmin)
