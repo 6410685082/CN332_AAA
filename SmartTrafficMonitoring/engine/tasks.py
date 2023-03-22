3177ca6b97edb9cb5e97cde92fe1294b19470f32
Chawanrat Gaysornkaew <6310611014@cn102>
2023-03-22 13:44:44 +0700

    can detect with celery

--- SmartTrafficMonitoring/engine/tasks.py
+++ SmartTrafficMonitoring/engine/tasks.py
@@ -1,14 +1,9 @@
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-from django.core.mail import get_connection
 from django.contrib.auth.models import User
-from engine.arial_car_track.ooad import detect_engine
  
 from celery import shared_task
 from celery.utils.log import get_task_logger
- 
+
 logger = get_task_logger(__name__)
  
 @shared_task()
@@ -18,7 +13,9 @@ def thirty_second_func():
 
 @shared_task
 def process():
-    detect_engine.detect_engine()
+    from ooad import detect_engine
+    d = detect_engine()
+    d.detect_engine()
     return "Done"
 
 @shared_task
