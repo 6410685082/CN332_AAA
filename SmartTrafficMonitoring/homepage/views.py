26761a2dddd718b45fdabae1a97526e7a9f96e20
Chanathinad Phispratuang <6310610941@cn103>
2023-03-22 17:46:26 +0700

    Add Homepage

---
+++ SmartTrafficMonitoring/homepage/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.files.storage import FileSystemStorage
+from django.utils import timezone
+from django.utils import dateformat
+from django.db.models import Q
+
+import sys
+sys.path.append("../user")
+
+from user.models import UserInfo
+
+@login_required(login_url='/user/login')
+def index(request):
+    return render(request, 'homepage/homepage.html')
