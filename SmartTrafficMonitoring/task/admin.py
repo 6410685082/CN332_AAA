e3028c1a3f9df2808874890199859a832b5e4651
Phattarakorn Kaewchukul <6310611030@cn103>
2023-03-29 21:28:56 +0700

    edit upload loop.txt

--- SmartTrafficMonitoring/task/admin.py
+++ SmartTrafficMonitoring/task/admin.py
@@ -7,5 +7,10 @@ class StatusAdmin(admin.ModelAdmin):
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['name', 'location', 'loop', 'input_vdo', 'output_vdo', 'status_id', 'note', 'preset', 'created_by', 'created_at', 'updated_at']
 
+class UploadFileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'video_file', 'loop_txt_file']
+
 admin.site.register(Status, StatusAdmin)
 admin.site.register(Task, TaskAdmin)
+admin.site.register(UploadFile, UploadFileAdmin)
+
