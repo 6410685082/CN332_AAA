60bed867d50ff24cf5676d4e41e221db20932904
Chanathinad Phispratuang <6310610941@cn103>
2023-05-10 20:06:57 +0700

    redirect

--- SmartTrafficMonitoring/user/views.py
+++ SmartTrafficMonitoring/user/views.py
@@ -22,7 +22,7 @@ def login_view(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse('homepage:index'))
+            return HttpResponseRedirect(reverse('task:index'))
         else:
             return render(request, log_in, {
                 'message': 'Invalid credentials.'
